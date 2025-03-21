3bc2f258e91349813be6e5279c87dda131928b0d
pippobura <filippoburato13@gmail.com>
2025-03-21 18:29:18 +0100

    CI corretta 8

---
+++ src/test/java/ModoloBuratBanca/BancaTest.java
@@ -0,0 +1,51 @@
+import java.time.LocalDate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BancaTest {
+
+  double contoStipendiato;
+  double contoConInvestimento;
+  double contoBancaPrima;
+  double contoPortafoglioPrima;
+
+  @BeforeEach
+  void setUp() {
+    Utente u1 = new Utente("Giorgio", "1234");
+    GestoreUtenti.utenti.add(u1);
+    contoBancaPrima = GestoreUtenti.utenti.elementAt(0).getContoBanca();
+    contoPortafoglioPrima = GestoreUtenti.utenti.elementAt(0).getContoPortafoglio();
+    Investimento i1 = new Investimento(100, 1);
+    GestoreUtenti.utenti.elementAt(0).investimenti.add(i1);
+    contoStipendiato = contoPortafoglioPrima + 1000;
+    contoConInvestimento =
+        contoBancaPrima
+            + GestoreUtenti.utenti.elementAt(0).investimenti.elementAt(0).getRendimento();
+  }
+
+  @Test
+  void testAvanzaMese() {
+    Banca.avanzaTempo();
+    Assertions.assertEquals(
+        LocalDate.of(2025, 2, 1), Banca.dataAttuale, "Il mese dovrebbe essere aumentato di 1 (2)");
+  }
+
+  @Test
+  void testStipendia() {
+    Banca.avanzaTempo();
+    Assertions.assertEquals(
+        contoStipendiato,
+        GestoreUtenti.utenti.elementAt(0).getContoPortafoglio(),
+        "Dovrebbe essere aumentato di 1000");
+  }
+
+  @Test
+  void testSottraiMese() {
+    Banca.avanzaTempo();
+    Assertions.assertEquals(
+        contoConInvestimento,
+        GestoreUtenti.utenti.elementAt(0).getContoBanca(),
+        "Dovrebbe essere aggiunto il rendimento dell'investimento");
+  }
+}
